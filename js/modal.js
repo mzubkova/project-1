416552fae663622ff5845beaed1f13c88b890a5d
Anastasia_Fadeeva <anastasiia.fadieieva@nure.ua>
2021-04-02 13:02:22 +0300

    second okno

---
+++ js/modal.js
@@ -0,0 +1,6 @@
+//questions-list__delete-btn => button close
+
+//var button = document.querySelector("questions-list__delete-btn");
+// button.addEventListener("click", function () {
+//     console.log("Button clicked.");
+// });
