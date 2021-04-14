2d65162e88c2f28619442bdacfabf73434d412ea
mzubkova <mary010118@gmail.com>
2021-04-15 00:15:21 +0300

    added filters, modal js, server

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -1,23 +1,43 @@
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.getElementById('btnClose'),
+//     modal: document.getElementById('modal'),
+//     dropDownBtn: document.getElementById('dropDown')
+// };
 
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.getElementById('btnClose'),
-    modal: document.getElementById('modal'),
-    dropDownBtn: document.getElementById('dropDown')
+// refs.openModalBtn.addEventListener("click", toggleModal);
+// refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//  function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
+
+// var backdrop = document.getElementsByClassName("backdrop")[0];
+var modal = document.getElementById("modal");
+var modalContent = document.querySelector(".modal");
+var modalOpen = document.getElementById("modal-btn");
+var modalClose = document.getElementById("btnClose");
+
+modalOpen.onclick = function () {
+  modal.style.display = "block";
+  modalContent.style.display = "block";
+  modalContent.style.zIndex = 99;
 };
-  
 
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
-  
- function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+modalClose.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+};
--- dist/js/questions.js
+++ dist/js/questions.js
@@ -15,20 +15,50 @@ function getQuestions(url) {
 
 getQuestions("http://localhost:8000/questions")
   .then((questions) => {
+    console.log("questions", questions);
     renderAllQuestions(questions);
+
+    var cards = document.querySelectorAll(".questions-list__card");
+    var toggleTheme = document.getElementById("theme");
+    var cardEmpty = document.querySelector(".questions-list--empty");
+
+    toggleTheme.addEventListener("change", function () {
+      var chosenTheme = this[this.selectedIndex].value;
+      var chosenFile = this[this.selectedIndex].value;
+
+      Array.prototype.forEach.call(cards, function (card, i) {
+        var topic = questions[i].topic;
+        var type = questions[i].type;
+        if (chosenTheme === topic && chosenFile === type) {
+          card.style.display = "inline-block";
+        } else {
+          cardEmpty.style.display = "inline-block";
+          card.style.display = "none";
+          setTimeout(function () {
+            location.reload();
+          }, 3000);
+        }
+      });
+    });
   })
   .catch((err) => console.log("error", err));
 
-var listContainer = document.querySelector(".questions-list");
-listContainer.addEventListener("click", onDeleteHandler);
-var select = document.querySelector(".questions-box__option");
-console.log(select);
-var option = document.querySelector(".questions-box__option");
-console.log(select);
+// var form = document.forms.addInvoice;
+// const inputNumber = form.elements.number;
+// const inputCreate = form.elements["date-created"];
+// const inputSupply = form.elements["date-supplied"];
+// const inputComment = form.elements.comment;
+
+// listContainer.addEventListener("click", onDeleteHandler);
+// var select = document.querySelector(".questions-box__option");
+// console.log(select.value);
+// var option = document.querySelector(".questions-box__option");
+// console.log(select.value);
 
 // form.addEventListener("submit", onFormSubmitHandler);
 
 function renderAllQuestions(questionsList) {
+  var listContainer = document.querySelector(".questions-list");
   if (!questionsList) {
     console.error("Pass the list of questions!");
     return;
@@ -36,6 +66,7 @@ function renderAllQuestions(questionsList) {
   console.log(questionsList);
   // console.log(questionsList[0].iso);
   // console.log(questionsList[0].answer);
+
   var fragment = document.createDocumentFragment();
   questionsList.forEach((question) => {
     var temp = listItemTemplate(question);
@@ -67,15 +98,12 @@ function listItemTemplate({ _id, topic, type, question, answer, iso } = {}) {
   var div = document.createElement("div");
   div.classList.add("questions-list__card");
   div.setAttribute("data-question-id", _id);
-
   var elemTopic = document.createElement("h2");
   elemTopic.textContent = topic;
   elemTopic.classList.add("questions-list__title", "title--h2");
-
   var elemSystem = document.createElement("span");
   elemSystem.textContent = type;
   elemSystem.classList.add("questions-list__type", "file-system");
-
   var elemAnswer = document.createElement("span");
   elemAnswer.textContent = answer;
   if (answer === true) {
@@ -83,18 +111,15 @@ function listItemTemplate({ _id, topic, type, question, answer, iso } = {}) {
   } else {
     elemAnswer.classList.add("questions-list__type", "false");
   }
-
   var elemDate = document.createElement("span");
   elemDate.textContent = iso;
   elemDate.classList.add("date");
-
   var elemQuestion = document.createElement("p");
   elemQuestion.textContent = question;
   elemQuestion.classList.add("questions-list__card-text");
 
   // var deleteBtnBox = document.createElement("div");
   // deleteBtnBox.classList.add("questions-list__delete", "d-flex");
-
   var deleteBtn = document.createElement("button");
   deleteBtn.classList.add("questions-list__delete-btn");
 
@@ -156,29 +181,23 @@ function listItemTemplate({ _id, topic, type, question, answer, iso } = {}) {
 //   return { ...newInvoice };
 // }
 
-function removeQuestion(id) {
-  var isConfirm = confirm("You want to delete question?");
-  if (!isConfirm) return isConfirm;
-  delete getQuestions.id;
-  return isConfirm;
-}
+// function removeQuestion(id) {
+//   var isConfirm = confirm("You want to delete question?");
+//   if (!isConfirm) return isConfirm;
+//   delete getQuestions.id;
+//   return isConfirm;
+// }
 
-function removeQuestionsFromHtml(confirmed, el) {
-  if (!confirmed) return;
-  el.remove();
-}
+// function removeQuestionsFromHtml(confirmed, el) {
+//   if (!confirmed) return;
+//   el.remove();
+// }
 
-function onDeleteHandler({ target }) {
-  if (target.classList.contains("btn--remove")) {
-    var parent = target.closest("[data-question-id]");
-    var id = parent.dataset.questionId;
-    var confirmed = removeQuestion(id);
-    removeQuestionsFromHtml(confirmed, parent);
-  }
-}
-
-function showSelect(e) {
-  e.preventDefault();
-  getQuestions().then(renderAllQuestions);
-}
-select.addEventListener("click", showSelect);
+// function onDeleteHandler({ target }) {
+//   if (target.classList.contains("btn--remove")) {
+//     var parent = target.closest("[data-question-id]");
+//     var id = parent.dataset.questionId;
+//     var confirmed = removeQuestion(id);
+//     removeQuestionsFromHtml(confirmed, parent);
+//   }
+// }
